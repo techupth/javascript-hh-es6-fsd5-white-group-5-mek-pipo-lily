a157c0ca5f2c9166123511cc1d7e4547e0269137
Yupaporn R. <lyupaporn.r@gmail.com>
2023-12-13 11:01:10 +0700

    feat(es6): add completed exercise

--- ex-6.js
+++ ex-6.js
@@ -1,4 +1,9 @@
 let height = undefined;
 let result;
 
+function isHeightUndefined(height) {
+  result = height ?? "Height is not defined";
+  return result;
+}
+result = isHeightUndefined(height);
 console.log(result); //  Result ควรจะได้ออกมาเป็น “Height is not defined”
